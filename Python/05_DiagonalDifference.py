81905d8bfd8faedf2b7b32b02542988ba986b8ab
Carlos David Páez Ferreira <carlos.paezf@usantoto.edu.co>
2022-02-24 12:52:12 -0500

    Diagonal Difference - PY

---
+++ Python/05_DiagonalDifference.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Mon Feb 21 2022
+
+@author: Carlos Páez
+'''
+
+#
+# Complete the `diagonalDifference` function below
+#
+# The function is expected to return a INTEGER
+# The function accepts 2D_INTEGER_ARRAY ar as parameter
+#
+
+def diagonalDifference(arr):    
+    """
+    Given a square matrix, calculate the absolute difference between the sums of its diagonals
+    
+    :param arr: an array of integers
+    :return: The absolute value of the difference between the sum of the diagonals.
+    """
+    left_to_right = 0
+    right_to_left = 0
+
+    x, y = 0, len(arr) - 1
+
+    for i in range(len(arr)):
+        left_to_right += arr[i][x]
+        right_to_left += arr[i][y]
+        x += 1
+        y -= 1
+
+    return abs(left_to_right - right_to_left)
+
+
+if __name__ == '__main__':
+    fptr = open('./output.txt', 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
