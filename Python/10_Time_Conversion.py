36f8c00bd068411571431dc58e81878c2bbe5558
Carlos David Páez Ferreira <carlos.paezf@usantoto.edu.co>
2022-04-29 18:56:23 -0500

    Time Conversion - PY

--- Python/10_Time_Conversion.py
+++ Python/10_Time_Conversion.py
@@ -20,13 +20,13 @@ def timeConversion(s):
     :param s: a string representing time in 12 hour format
     :return: The time in 24 hour format.
     """
-    if s[:-2] == "AM":
-        if s[:2] == '12':
-            return '00' + s[2:-2]
+    if s[-2:] == "AM":
+        if s[:2] == "12":
+            return "00" + s[2:-2]
         else:
             return s[:-2]
     else:
-        if s[:2] == '12':
+        if s[:2] == "12":
             return s[:-2]
         else:
             return str(int(s[:2]) + 12) + s[2:-2]
