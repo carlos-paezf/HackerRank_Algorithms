cb48a7746c63ac2a646d9401f8491c37ddfa9c9e
carlos-paezf <cpaezferreira@gmail.com>
2023-02-22 12:01:19 -0500

    Add Number Line Jumps JavaScript

---
+++ JavaScript/13-Number_Line_Jumps.js
@@ -0,0 +1,80 @@
+/**
+ * Created on Wed Feb 21 2023
+ * 
+ * @author Carlos Páez
+ */
+'use strict';
+
+const fs = require('fs')
+
+process.stdin.resume()
+process.stdin.setEncoding('utf-8')
+
+let inputString = ''
+let currentLine = 0
+
+/* Reading the input from the console. */
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin
+})
+
+/* Reading the input from the console. */
+process.stdin.on('end', function () {
+    inputString = inputString.split('n')
+    main()
+})
+
+
+/**
+ * It reads the input string and returns the current line.
+ * @returns The current line of the inputString.
+ */
+function readLine () {
+    return inputString[ currentLine++ ]
+}
+
+
+/*
+ * Complete the `kangaroo` function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER x1
+ *  2. INTEGER v1
+ *  3. INTEGER x2
+ *  4. INTEGER v2
+ */
+
+function kangaroo (x1, v1, x2, v2) {
+    const maxLimit = 10000
+
+    const invalidCondition = (
+        x1 < 0 || x1 > maxLimit || x2 < 0 || x2 > maxLimit ||
+        v1 < 0 || v1 > maxLimit || v2 < 0 || v2 > maxLimit ||
+        x1 > x2 || v1 < v2
+    )
+
+    if (invalidCondition) return 'NO'
+
+    if (v1 !== v2 && (x2 - x1) % (v2 - v1) === 0) return 'YES'
+}
+
+function main () {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const x1 = parseInt(firstMultipleInput[ 0 ], 10);
+
+    const v1 = parseInt(firstMultipleInput[ 1 ], 10);
+
+    const x2 = parseInt(firstMultipleInput[ 2 ], 10);
+
+    const v2 = parseInt(firstMultipleInput[ 3 ], 10);
+
+    const result = kangaroo(x1, v1, x2, v2);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
